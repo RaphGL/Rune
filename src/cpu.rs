b929256bbfced7df72f949f591e9c62f00325056
RaphGL <raphfl.dev@gmail.com>
2023-05-20 16:50:22 -0100

    added and opcode

--- src/cpu.rs
+++ src/cpu.rs
@@ -48,8 +48,6 @@ impl CPU {
 
     pub fn cycle(&mut self) {}
 
-    // ADC instructions
-    //
     /// ADC #$44
     fn adc69(&mut self, operand: u8) {
         let (inum, overflowed1) = unsafe {
@@ -95,7 +93,7 @@ impl CPU {
 
     /// ADC $44, X
     fn adc75(&mut self, operand: u8) {
-        let operand = self.ram[operand as usize] + self.x;
+        let operand = self.ram[(operand + self.x) as usize];
         self.adc69(operand);
     }
 
@@ -107,13 +105,13 @@ impl CPU {
 
     /// ADC $4400, X
     fn adc7d(&mut self, operand: u16) {
-        let operand = self.ram[operand as usize] + self.x;
+        let operand = self.ram[(operand + self.x as u16) as usize];
         self.adc69(operand);
     }
 
     /// ADC $4400,Y
     fn adc79(&mut self, operand: u16) {
-        let operand = self.ram[operand as usize] + self.y;
+        let operand = self.ram[(operand + self.y as u16) as usize];
         self.adc69(operand);
     }
 
@@ -129,8 +127,71 @@ impl CPU {
     fn adc71(&mut self, operand: u8) {
         let addr: u16 =
             self.ram[(operand) as usize] as u16 | (self.ram[(operand + 1) as usize] as u16) << 8;
-        let operand = self.ram[addr as usize] + self.y;
+        let operand = self.ram[(addr + self.y as u16) as usize];
         self.adc69(operand);
+    }
+
+    /// AND #$44
+    fn and29(&mut self, operand: u8) {
+        self.a &= operand;
+
+        if self.a == 0 {
+            self.zero = true;
+        } else {
+            self.zero = false;
+        }
+
+        if self.a >> 7 == 1 {
+            self.negative = true;
+        } else {
+            self.negative = false;
+        }
+    }
+
+    /// AND $44
+    fn and25(&mut self, operand: u8) {
+        let operand = self.ram[operand as usize];
+        self.and29(operand);
+    }
+
+    /// AND $44,X
+    fn and35(&mut self, operand: u8) {
+        let operand = self.ram[(operand + self.x) as usize];
+        self.and29(operand);
+    }
+
+    /// AND $4400
+    fn and2d(&mut self, operand: u16) {
+        let operand = self.ram[operand as usize];
+        self.and29(operand);
+    }
+
+    /// AND $4400,X
+    fn and3d(&mut self, operand: u16) {
+        let operand = self.ram[(operand + self.x as u16) as usize];
+        self.and29(operand);
+    }
+
+    /// AND $4400,Y
+    fn and39(&mut self, operand: u16) {
+        let operand = self.ram[(operand + self.y as u16) as usize];
+        self.and29(operand);
+    }
+
+    /// AND ($44,X)
+    fn and21(&mut self, operand: u8) {
+        let addr: u16 = self.ram[(operand + self.x) as usize] as u16
+            | (self.ram[(operand + self.x + 1) as usize] as u16) << 8;
+        let operand = self.ram[addr as usize];
+        self.and29(operand);
+    }
+
+    /// AND ($44),Y
+    fn and31(&mut self, operand: u8) {
+        let addr: u16 =
+            self.ram[operand as usize] as u16 | (self.ram[(operand + 1) as usize] as u16) << 8;
+        let operand = self.ram[(addr + self.y as u16) as usize];
+        self.and29(operand);
     }
 }
 
@@ -159,10 +220,10 @@ mod tests {
         cpu.adc65(0x44);
         assert!(cpu.a == 32);
 
-        cpu.a = 0;
-        cpu.x = 4;
+        cpu.a = 4;
         cpu.ram[0xff] = 50;
-        cpu.adc75(0xff);
+        cpu.x = 0x2;
+        cpu.adc75(0xfd);
         assert!(cpu.a == 54);
 
         cpu.ram[0x1ee] = 244;
@@ -172,15 +233,15 @@ mod tests {
 
         cpu.ram[0x1ee] = 156;
         cpu.a = 2;
-        cpu.x = 3;
-        cpu.adc7d(0x1ee);
-        assert!(cpu.a == 161);
+        cpu.x = 0xee;
+        cpu.adc7d(0x100);
+        assert!(cpu.a == 158);
 
         cpu.ram[0x7ee] = 70;
         cpu.a = 90;
-        cpu.y = 3;
-        cpu.adc79(0x7ee);
-        assert!(cpu.a == 163);
+        cpu.y = 0xe0;
+        cpu.adc79(0x70e);
+        assert!(cpu.a == 160);
 
         cpu.ram[0x45] = 0xab;
         cpu.ram[0x46] = 0x01;
@@ -190,12 +251,73 @@ mod tests {
         cpu.adc61(0x44);
         assert!(cpu.a == 222);
 
-        cpu.ram[100] = 0x3e;
-        cpu.ram[101] = 0x05;
-        cpu.ram[0x053e] = 111;
         cpu.a = 0;
+        cpu.ram[0xaa] = 0xca;
+        cpu.ram[0xab] = 0x01;
         cpu.y = 3;
-        cpu.adc71(100);
-        assert!(cpu.a == 114);
+        cpu.ram[0x01cd] = 111;
+        cpu.adc71(0xaa);
+        println!("{}", cpu.a);
+        assert!(cpu.a == 111);
+    }
+
+    #[test]
+    fn test_and_opcodes() {
+        let mut cpu = CPU::default();
+        cpu.a = 0x0f;
+        cpu.and29(0xf0);
+        assert!(cpu.a == 0);
+        assert!(cpu.zero == true);
+        cpu.a = 0xfa;
+        cpu.and29(0x0f);
+        assert!(cpu.a == 0x0a);
+        cpu.a = 0xff;
+        cpu.and29(0xff);
+        assert!(cpu.negative == true);
+
+        // the following opcodes all use addc69 underneath so there's no need to test flags again
+        cpu.ram[0xaa] = 0xf0;
+        cpu.a = 0xea;
+        cpu.and25(0xaa);
+        assert!(cpu.a == 0xe0);
+
+        cpu.ram[40] = 0xff;
+        cpu.a = 0xEE;
+        cpu.x = 2;
+        cpu.and35(38);
+        assert!(cpu.a == 0xee);
+
+        cpu.ram[2000] = 0x0f;
+        cpu.a = 0xac;
+        cpu.and2d(2000);
+        assert!(cpu.a == 0xc);
+
+        cpu.ram[2005] = 0x56;
+        cpu.a = 0xf0;
+        cpu.x = 5;
+        cpu.and3d(2000);
+        assert!(cpu.a == 0x50);
+
+        cpu.ram[2046] = 0xa7;
+        cpu.a = 0x0f;
+        cpu.y = 10;
+        cpu.and39(2036);
+        assert!(cpu.a == 0x7);
+
+        cpu.ram[10] = 0x0e;
+        cpu.ram[11] = 0x1;
+        cpu.ram[0x10e] = 0xcc;
+        cpu.a = 0xf0;
+        cpu.x = 3;
+        cpu.and21(7);
+        assert!(cpu.a == 0xc0);
+
+        cpu.ram[10] = 0x0f;
+        cpu.ram[11] = 0x02;
+        cpu.ram[0x211] = 0xdd;
+        cpu.a = 0x0f;
+        cpu.y = 2;
+        cpu.and31(10);
+        assert!(cpu.a == 0xd);
     }
 }
