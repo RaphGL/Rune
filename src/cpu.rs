f987203e3d78539ce7bbe25a1d3aa3bcf1eaa708
RaphGL <raphfl.dev@gmail.com>
2023-05-20 12:08:01 -0100

    completed adc opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,65 +1,4 @@
-mod status {
-    pub const NEGATIVE: u8 = 0b1000_0000;
-    pub const OVERFLOW: u8 = 0b0100_0000;
-    pub const DECIMAL: u8 = 0b0000_1000;
-    pub const INTERRUPT_DISABLE: u8 = 0b0000_0100;
-    pub const ZERO: u8 = 0b0000_0010;
-    pub const CARRY: u8 = 0b0000_0001;
-    pub const B: u8 = 0b0011_0000;
-}
-
-struct CPUFlags(u8);
-
-impl CPUFlags {
-    fn get_negative(&self) -> u8 {
-        (self.0 & status::NEGATIVE) >> 7
-    }
-    fn set_negative(&mut self, state: u8) {
-        self.0 = (state << 7) & status::NEGATIVE;
-    }
-
-    fn get_overflow(&self) -> u8 {
-        (self.0 & status::OVERFLOW) >> 6
-    }
-    fn set_overflow(&mut self, state: u8) {
-        self.0 = (state << 6) & status::OVERFLOW;
-    }
-
-    fn get_b(&self) -> u8 {
-        (self.0 & status::B) >> 4
-    }
-    fn set_b(&mut self, state: u8) {
-        self.0 = (state << 4) & status::B;
-    }
-
-    fn get_decimal(&self) -> u8 {
-        (self.0 & status::DECIMAL) >> 3
-    }
-    fn set_decimal(&mut self, state: u8) {
-        self.0 = (state << 3) & status::DECIMAL;
-    }
-
-    fn get_interrupt(&self) -> u8 {
-        (self.0 & status::INTERRUPT_DISABLE) >> 2
-    }
-    fn set_interrupt(&mut self, state: u8) {
-        self.0 = (state << 2) & status::INTERRUPT_DISABLE;
-    }
-
-    fn get_zero(&self) -> u8 {
-        (self.0 & status::ZERO) >> 1
-    }
-    fn set_zero(&mut self, state: u8) {
-        self.0 = (state << 1) & status::ZERO;
-    }
-
-    fn get_carry(&self) -> u8 {
-        self.0 & status::CARRY
-    }
-    fn set_carry(&mut self, state: u8) {
-        self.0 = state & status::CARRY;
-    }
-}
+use std::mem;
 
 pub struct CPU {
     ram: [u8; 2048],
@@ -73,8 +12,15 @@ pub struct CPU {
     x: u8,
     /// register y
     y: u8,
-    /// processor status
-    p: CPUFlags,
+
+    // processor status flags
+    carry: u8,
+    negative: bool,
+    overflow: bool,
+    decimal: bool,
+    interrupt_disable: bool,
+    zero: bool,
+    b: bool,
 }
 
 impl Default for CPU {
@@ -86,7 +32,13 @@ impl Default for CPU {
             a: 0,
             x: 0,
             y: 0,
-            p: CPUFlags(0),
+            negative: false,
+            overflow: false,
+            decimal: false,
+            interrupt_disable: false,
+            zero: false,
+            carry: 0,
+            b: false,
         }
     }
 }
@@ -97,139 +49,88 @@ impl CPU {
     pub fn cycle(&mut self) {}
 
     // ADC instructions
+    //
+    /// ADC #$44
     fn adc69(&mut self, operand: u8) {
-        let (num, carried) = operand.overflowing_add(self.p.get_carry());
-        self.a += num;
-        if carried {
-            self.p.set_carry(1);
+        let (inum, overflowed1) = unsafe {
+            mem::transmute::<u8, i8>(operand).overflowing_add(mem::transmute::<u8, i8>(self.carry))
+        };
+        let (_, overflowed2) = unsafe { inum.overflowing_add(mem::transmute::<u8, i8>(self.a)) };
+
+        let (num, carried1) = operand.overflowing_add(self.carry);
+        let (res, carried2) = num.overflowing_add(self.a);
+
+        self.a = res;
+
+        if carried1 || carried2 {
+            self.carry = 1;
+        } else {
+            self.carry = 0;
+        }
+
+        if overflowed1 || overflowed2 {
+            self.overflow = true;
+        } else {
+            self.overflow = false;
         }
 
         if self.a == 0 {
-            self.p.set_zero(1);
+            self.zero = true;
+        } else {
+            self.zero = false;
         }
 
         if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
+            self.negative = true;
+        } else {
+            self.negative = false;
         }
     }
 
+    /// ADC $44
     fn adc65(&mut self, operand: u8) {
-        let (num, overflowed) = self.ram[operand as usize].overflowing_add(self.p.get_carry());
-        self.a += num;
-        if overflowed {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let operand = self.ram[operand as usize];
+        self.adc69(operand);
     }
 
+    /// ADC $44, X
     fn adc75(&mut self, operand: u8) {
-        let (num, overflowed1) = self.ram[operand as usize].overflowing_add(self.x);
-        let (num, overflowed2) = num.overflowing_add(self.p.get_carry());
-        self.a += num;
-        if overflowed1 || overflowed2 {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let operand = self.ram[operand as usize] + self.x;
+        self.adc69(operand);
     }
 
+    /// ADC $4400
     fn adc6d(&mut self, operand: u16) {
-        let (num, overflowed) = self.ram[operand as usize].overflowing_add(self.p.get_carry());
-        self.a += num;
-        if overflowed {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let operand = self.ram[operand as usize];
+        self.adc69(operand);
     }
 
+    /// ADC $4400, X
     fn adc7d(&mut self, operand: u16) {
-        let (num, overflowed1) = self.ram[operand as usize].overflowing_add(self.x);
-        let (num, overflowed2) = num.overflowing_add(self.p.get_carry());
-        self.a += num;
-        if overflowed1 || overflowed2 {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let operand = self.ram[operand as usize] + self.x;
+        self.adc69(operand);
     }
 
+    /// ADC $4400,Y
     fn adc79(&mut self, operand: u16) {
-        let (num, overflowed1) = self.ram[operand as usize].overflowing_add(self.y);
-        let (num, overflowed2) = num.overflowing_add(self.p.get_carry());
-        self.a += num;
-        if overflowed1 || overflowed2 {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let operand = self.ram[operand as usize] + self.y;
+        self.adc69(operand);
     }
 
+    /// ADC ($44,X)
     fn adc61(&mut self, operand: u8) {
-        let operand = operand as usize;
-        let num = self.ram[operand] + self.x;
-        let (num, overflowed) = self.ram[num as usize].overflowing_add(self.x);
-        self.a += num;
-        if overflowed {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let addr: u16 = self.ram[(operand + self.x) as usize] as u16
+            | (self.ram[(operand + self.x + 1) as usize] as u16) << 8;
+        let operand = self.ram[addr as usize];
+        self.adc69(operand);
     }
 
+    /// ADC ($44),Y
     fn adc71(&mut self, operand: u8) {
-        let operand = operand as usize;
-        let num = self.ram[operand] & (self.ram[operand + 1] << 8);
-        let (num, overflowed) = num.overflowing_add(self.y);
-        self.a += num;
-        if overflowed {
-            self.p.set_carry(1);
-        }
-
-        if self.a == 0 {
-            self.p.set_zero(1);
-        }
-
-        if (self.a >> 7) == 1 {
-            self.p.set_negative(1);
-        }
+        let addr: u16 =
+            self.ram[(operand) as usize] as u16 | (self.ram[(operand + 1) as usize] as u16) << 8;
+        let operand = self.ram[addr as usize] + self.y;
+        self.adc69(operand);
     }
 }
 
@@ -238,63 +139,63 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cpu_flags() {
-        let mut p = CPU::default().p;
-        assert!(p.get_carry() == 0);
-        p.set_carry(1);
-        assert!(p.get_carry() == 1);
-
-        assert!(p.get_b() == 0b00);
-        p.set_b(0b11);
-        assert!(p.get_b() == 0b11);
-
-        assert!(p.get_zero() == 0);
-        p.set_zero(1);
-        assert!(p.get_zero() == 1);
-
-        assert!(p.get_decimal() == 0);
-        p.set_decimal(1);
-        assert!(p.get_decimal() == 1);
-
-        assert!(p.get_negative() == 0);
-        p.set_negative(1);
-        assert!(p.get_negative() == 1);
-
-        assert!(p.get_overflow() == 0);
-        p.set_overflow(1);
-        assert!(p.get_overflow() == 1);
-
-        assert!(p.get_interrupt() == 0);
-        p.set_interrupt(1);
-        assert!(p.get_interrupt() == 1);
-    }
-
-    #[test]
     fn test_adc_opcodes() {
         let mut cpu = CPU::default();
-        cpu.adc69(0x44);
-        assert!(cpu.a == 0x44);
 
-        cpu.ram[0x44] = 0x69;
-        cpu.adc65(0x44);
-        assert!(cpu.a == 0x69 + 0x44);
-
-        cpu.x = 0x21;
-        cpu.ram[0x20] = 0x40;
-        cpu.a = 0;
-        cpu.adc75(0x20);
-        assert!(cpu.a == 0x61);
-
-        cpu.ram[0x07ff] = 90;
-        cpu.adc6d(0x07ff);
-        assert!(cpu.a == 0x61 + 90);
-
-        cpu.a = 0;
-        cpu.ram[0x7fe] = 0xff;
-        cpu.x = 1;
-        cpu.adc7d(0x7fe);
+        cpu.a = 255;
+        cpu.adc69(1);
         assert!(cpu.a == 0);
-        // TODO add carry and overflow flags to adc
-        assert!(cpu.p.get_carry() == 1);
+        assert!(cpu.carry == 1);
+        assert!(cpu.zero == true);
+        cpu.adc69(254);
+        assert!(cpu.negative == true);
+        cpu.a = unsafe { mem::transmute::<i8, u8>(i8::MAX) };
+        cpu.adc69(1);
+        assert!(cpu.overflow == true);
+
+        // the following opcodes all use addc69 underneath so there's no need to test flags again
+        cpu.ram[0x44] = 29;
+        cpu.a = 3;
+        cpu.adc65(0x44);
+        assert!(cpu.a == 32);
+
+        cpu.a = 0;
+        cpu.x = 4;
+        cpu.ram[0xff] = 50;
+        cpu.adc75(0xff);
+        assert!(cpu.a == 54);
+
+        cpu.ram[0x1ee] = 244;
+        cpu.a = 2;
+        cpu.adc6d(0x1ee);
+        assert!(cpu.a == 246);
+
+        cpu.ram[0x1ee] = 156;
+        cpu.a = 2;
+        cpu.x = 3;
+        cpu.adc7d(0x1ee);
+        assert!(cpu.a == 161);
+
+        cpu.ram[0x7ee] = 70;
+        cpu.a = 90;
+        cpu.y = 3;
+        cpu.adc79(0x7ee);
+        assert!(cpu.a == 163);
+
+        cpu.ram[0x45] = 0xab;
+        cpu.ram[0x46] = 0x01;
+        cpu.ram[0x01ab] = 222;
+        cpu.a = 0;
+        cpu.x = 1;
+        cpu.adc61(0x44);
+        assert!(cpu.a == 222);
+
+        cpu.ram[100] = 0x3e;
+        cpu.ram[101] = 0x05;
+        cpu.ram[0x053e] = 111;
+        cpu.a = 0;
+        cpu.y = 3;
+        cpu.adc71(100);
+        assert!(cpu.a == 114);
     }
 }
